06a52be96d8ea46f99b7bb088eb933d77ca61574
SatwikDubey <dubeysatwik0@gmail.com>
2025-06-12 11:32:20 +0530

    Update index.js

--- 07_Building_HTTP_Server/index.js
+++ 07_Building_HTTP_Server/index.js
@@ -1,7 +1,7 @@
 const http = require("http");
 const fs = require("fs");
 
-const myServer = http.createServer((req , res)=>{
+const myServer = http.createServer((req , res)=>{ // createServer create a web server for us
 
     const log = `${Date.now()} : ${req.url} : New Request Recieved \n`;
     fs.appendFile('log.txt' , log , (err , data)=>{
@@ -33,3 +33,4 @@ myServer.listen(8000, ()=>{
     console.log(`Server started at port no ${PORT} `);
 })
 
+// const myServer=http.createServer((req,res)=>{}); so here i have gave a fn to createserver to ye jo maine arrow fn pass kiya h ye responsible h incoming req ko process karne ke  liye 
