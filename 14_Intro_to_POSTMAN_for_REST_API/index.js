30742e9165cded0075be5e32742c5ae336571e89
SatwikDubey <dubeysatwik0@gmail.com>
2025-06-15 17:42:54 +0530

    Update index.js

--- 14_Intro_to_POSTMAN_for_REST_API/index.js
+++ 14_Intro_to_POSTMAN_for_REST_API/index.js
@@ -40,18 +40,61 @@ app.route('/api/users/:id').get((req, res)=>{
     res.json({status : 'Pending'})
 })
 
+/*
+Understanding fs.writeFile () part
 
+fs.writeFile(path, data, callback)
 
-app.post('/api/users' , (req , res)=>{
-    // TODO : Create new user
-    const  body = req.body;
-    users.push({...body , id:users.length+1});
-    fs.writeFile('./MOCK_DATA.json', JSON.stringify(users), (err , data)=>{
-        return res.json({status:"pending"});
-    })
-    console.log(body);
-    return res.json({status: "sucess", id:users.length});
+path: The file location (here: './MOCK_DATA.json')
+
+data: The content you want to write into the file (must be a string or buffer)
+
+callback: A function that runs after the write is complete (takes err and data as parameters)
+
+----
+You’re converting the users array (an in-memory JavaScript array) into a JSON string.
+
+users = [{ id: 1, name: "John" }];
+JSON.stringify(users) 
+// Output: '[{"id":1,"name":"John"}]'
+------
+
+ (err, data) => { ... }
+This is the callback function that gets executed after the file writing is attempted.
+
+err: If an error occurred during writing (like permission denied, disk full, etc.), this will contain the error.
+
+data: This is always undefined in fs.writeFile, because it doesn't return any actual data.
+
+*/
+
+fs.writeFile('./MOCK_DATA.json', JSON.stringify(users), (err) => {
+    if (err) {
+        console.error("Failed to write file:", err);
+        return res.status(500).json({ status: "error", message: "Failed to save user" });
+    }
+
+    return res.json({ status: "success", id: users.length });
 });
+
+/*
+✅ Summary of Correct Flow
+Receive data from client
+
+Add it to the users array
+
+Save that array to MOCK_DATA.json using fs.writeFile
+
+After writing:
+
+If there’s an error: respond with 500
+
+If successful: respond with "success" and new user ID
+*/
+
+-----
+
+
 
 // app.patch('/api/users/:id' , (req , res)=>{
 //     // TODO : Edit the user with id
